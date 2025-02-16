d32320fb0eabec9c8b6f54a462d0acfa2f5416be
qr96 <qr96@naver.com>
2025-02-16 19:29:07 +0900

    add FileManager, WebController

--- MakeApple/Assets/Scripts/GameA/Managers.cs
+++ MakeApple/Assets/Scripts/GameA/Managers.cs
@@ -10,10 +10,15 @@ namespace GameA
         public MainGameLogic mainGameLogic;
         public UIManager ui;
         public EventController eventController;
+        public FileManager fileManager;
+        public WebController webController;
 
         public static MainGameLogic MainLogic { get { return Instance?.mainGameLogic; } }
         public static UIManager UI { get { return Instance?.ui; } }
         public static EventController Event { get { return Instance?.eventController; } } 
+        public static FileManager File { get { return Instance?.fileManager; } }
+        public static WebController Web { get { return Instance?.webController; } }
+
         private void Awake()
         {
             Instance = this;
