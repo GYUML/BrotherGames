d96a172e932c2aa7b069cf1c080a0c3b81515557
qr96 <qr96@naver.com>
2025-02-17 22:20:48 +0900

    add MakeAccountPopup

---
+++ MakeApple/Assets/Scripts/GameA/UI/MakeAccountPopup.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+
+public class MakeAccountPopup : UIPopup
+{
+    public TMP_InputField nicknameField;
+    public KButton registerButton;
+
+    private void Start()
+    {
+        
+    }
+}
