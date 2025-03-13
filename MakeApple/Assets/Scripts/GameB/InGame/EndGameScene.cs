a4522fd03b9b579b0c49cdf049c30757188392dc
qr96 <qr96@naver.com>
2025-03-13 23:52:40 +0900

    mod EndGameScene

--- MakeApple/Assets/Scripts/GameB/InGame/EndGameScene.cs
+++ MakeApple/Assets/Scripts/GameB/InGame/EndGameScene.cs
@@ -6,11 +6,11 @@ public class EndGameScene : MonoBehaviour
 {
     public Animator playerAnimator;
 
-    public void Play()
+    public void Play(long maxHp, long damage)
     {
         playerAnimator.SetTrigger("2_Attack");
         playerAnimator.transform.position = new Vector2(0f, 8f);
         playerAnimator.transform.DOMoveY(-4.2f, 0.5f).SetEase(Ease.Linear)
-            .OnComplete(() => Managers.UI.ShowPopup<FallingGameResultPopup>().StartEndEffect(10000, 3000));
+            .OnComplete(() => Managers.UI.ShowPopup<FallingGameResultPopup>().StartEndEffect(maxHp, damage));
     }
 }
