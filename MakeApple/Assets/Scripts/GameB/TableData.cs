1535a6cac1c027a688651f5976e079af668361af
qr96 <qr96@naver.com>
2025-03-19 22:43:07 +0900

    mod gameB tabledata namespace

--- MakeApple/Assets/Scripts/GameB/TableData.cs
+++ MakeApple/Assets/Scripts/GameB/TableData.cs
@@ -1,31 +1,34 @@
 using UnityEngine;
 
-public class TableData : MonoBehaviour
+namespace GameB
 {
-    public long GetAttackPerLevel(int level)
+    public class TableData : MonoBehaviour
     {
-        return 10L + level * 5;
-    }
-
-    public double GetBonusAttackPer(float startHeight, float normalFallingSpeed, float recordTime)
-    {
-        if (normalFallingSpeed == 0)
+        public long GetAttackPerLevel(int level)
         {
-            Debug.LogError("[TableData] GetBonusAttackPer() normalFalling speed can't be zero.");
-            return 0d;
+            return 10L + level * 5;
         }
 
-        var bestTime = startHeight / (normalFallingSpeed * 2);
-        var normalTime = startHeight / normalFallingSpeed;
+        public double GetBonusAttackPer(float startHeight, float normalFallingSpeed, float recordTime)
+        {
+            if (normalFallingSpeed == 0)
+            {
+                Debug.LogError("[TableData] GetBonusAttackPer() normalFalling speed can't be zero.");
+                return 0d;
+            }
 
-        if (normalTime == bestTime)
+            var bestTime = startHeight / (normalFallingSpeed * 2);
+            var normalTime = startHeight / normalFallingSpeed;
+
+            if (normalTime == bestTime)
+                return 0d;
+
+            if (recordTime <= bestTime)
+                return 1d;
+            else if (recordTime < normalTime)
+                return 1d - (recordTime - bestTime) / (normalTime - bestTime);
+
             return 0d;
-
-        if (recordTime <= bestTime)
-            return 1d;
-        else if (recordTime < normalTime)
-            return 1d - (recordTime - bestTime) / (normalTime - bestTime);
-
-        return 0d;
+        }
     }
 }
